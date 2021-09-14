d0729b0284954103345da9061ff8d54222477e66
Minh-Quân Bui <contact.buiminhquan@gmail.com>
2021-09-14 18:06:36 +0200

    Ex4 : create HomePage component and add it to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
-  <div></div>
+  <HomePage></HomePage>
 </template>
 
 <script>
+import HomePage from './components/HomePage.vue'
 
 export default {
   name: 'App',
   components: {
+    HomePage,
   }
 }
 </script>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1> Homepage </h1>
+</template>
+
+<script>
+    export default {
+        name: 'HomePage',
+        components: {
+        }
+    }
+</script>
